6139456e0134e7fe30523db8e0fc7ba547e97c4d
Nathan Jones <tech.nathanjones@gmail.com>
2022-08-27 18:21:12 -0500

    initial

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+const codeLanguage = 'javascript'
+const code = `export default {
+  strategy: 'predictive',
+  engine: {
+    cpus: 12,
+    backups: ['./storage/cache.wtf'],
+  },
+}`
+
+export function Hero() {
+  return (
+    <div className="pt-48">
+    </div>
+  )
+}
