437458338789258410ad3c2e359286f75c2cbb1c
Nathan Jones <tech.nathanjones@gmail.com>
2022-08-28 11:30:58 -0500

    change

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,6 @@
 function LogomarkPaths() {
   return (
-    <g fill="none" stroke="#38BDF8" strokeLinejoin="round" strokeWidth={3}>
-      <path d="M10.308 5L18 17.5 10.308 30 2.615 17.5 10.308 5z" />
-      <path d="M18 17.5L10.308 5h15.144l7.933 12.5M18 17.5h15.385L25.452 30H10.308L18 17.5z" />
+    <g>
     </g>
   )
 }
@@ -10,7 +8,6 @@ function LogomarkPaths() {
 export function Logomark(props) {
   return (
     <svg aria-hidden="true" viewBox="0 0 36 36" fill="none" {...props}>
-      <LogomarkPaths />
     </svg>
   )
 }
